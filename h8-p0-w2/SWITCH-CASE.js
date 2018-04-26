263e4908b96fc040678ca19ca0eb9f7028894ac6
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-04-26 15:52:47 +0700

    Revised code according to suggestion from instructor, more efficiency

--- h8-p0-w2/SWITCH-CASE.js
+++ h8-p0-w2/SWITCH-CASE.js
@@ -1,48 +1,50 @@
 
 // Initialize variables
 var tanggal = "1"; // assign nilai variabel tanggal disini! (dengan angka antara 1 - 31)
-var bulan = "Desember"; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
+var bulan = "13"; // assign nilai variabel bulan disini! (dengan angka antara 1 - 12)
 var tahun = "1978"; // assign nilai variabel tahun disini! (dengan angka antara 1900 - 2200)
+var bulanConverted = null;
 
 // Switch Logic
 switch (bulan) {
   case "1":
-  	console.log(tanggal + " Januari " + tahun);
+  	bulanConverted = "Januari";
  		break;
   case "2":
-    console.log(tanggal + " Februari " + tahun);
+    bulanConverted = "Februari";
     break;
   case "3":
-    console.log(tanggal + " Maret " + tahun);
+    bulanConverted = "Maret";
    	break;
   case "4":
-    console.log(tanggal + " April " + tahun);
+    bulanConverted = "April";
     break;
   case "5":
-    console.log(tanggal + " Mei " + tahun);
+    bulanConverted = "Mei";
    	break;
   case "6":
-    console.log(tanggal + " Juni " + tahun);
+    bulanConverted = "Juni";
     break;
   case "7":
-    console.log(tanggal + " Juli " + tahun);
+    bulanConverted = "Juli";
     break;
   case "8":
-    console.log(tanggal + " Agustus " + tahun);
+    bulanConverted = "Agustus";
     break;
   case "9":
-    console.log(tanggal + " September " + tahun);
+    bulanConverted = "September";
     break;
   case "10":
-    console.log(tanggal + " Oktober " + tahun);
+    bulanConverted = "Oktober";
     break;
   case "11":
-    console.log(tanggal + " November " + tahun);
+    bulanConverted = "November";
     break;
   case "12":
-    console.log(tanggal + " Desember " + tahun);
-    break;
-  default:
-    console.log("Invalid!");
+    bulanConverted = "Desember";
     break;
 }
+
+if (bulanConverted) {
+console.log(tanggal + " " + bulanConverted + " " + tahun);
+} else {console.log("Invalid!");}
