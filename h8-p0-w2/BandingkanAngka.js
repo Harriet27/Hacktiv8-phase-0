13cee5def8aa334089e861d2a7bd3e7afa7c3410
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-04-27 14:55:57 +0700

    add BandingkanAngka.js

---
+++ h8-p0-w2/BandingkanAngka.js
@@ -0,0 +1,17 @@
+// Init comparaison function
+function bandingkanAngka(angka1, angka2) {
+  if (angka1 > angka2) {
+    return "FALSE";
+  } else if (angka2 > angka1) {
+    return "TRUE"
+  } else if (angka2 === angka1) {
+    return "-1";
+  }
+}
+
+// teste me!
+console.log(bandingkanAngka(5, 8)); // true
+console.log(bandingkanAngka(5, 3)); // false
+console.log(bandingkanAngka(4, 4)); // -1
+console.log(bandingkanAngka(3, 3)); // -1
+console.log(bandingkanAngka(17, 2)); // false
