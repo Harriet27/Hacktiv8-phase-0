f662b11ea592d1002be911b9989b86e65af852f8
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-05-06 18:55:13 +0700

    add comments at target-terdekat.js

--- h8-p0-w3/target-terdekat.js
+++ h8-p0-w3/target-terdekat.js
@@ -1,42 +1,57 @@
 function targetTerdekat(arr) {
 
-  var tempoArr = [];
-  var positionX, positionO;
-  var shortestDistance = 0;
+// init global variables
+  var tempoArr = []; // arr to contain sliced arr for distance measurement
+  var positionX, positionO; // arr to contain position of O and X
+  var shortestDistance = 0; // init shortestDistance
 
+// loop for measuring shortestDistance
   for (var i = 0; i <= arr.length - 1; i++) {
-    var inspector = arr[i];
-    var tempoDistance;
+    var inspector = arr[i]; // inspector var which will be used for comparison
+    var tempoDistance; // var to contain temporary distance, which will be used during comparisons
+
     if (inspector === "x") {
-      positionX = i;
+      positionX = i; // assigning position of X
     } else if (inspector === "o") {
-      positionO = i;
+      positionO = i; // assigning position of O
     }
+// var to contain typeof positionO and positionX
     var typeX = typeof positionX;
     var typeO = typeof positionO;
     // console.log("ini arr = " + arr);
     // console.log("ini typeof positionX = " + typeX + "| ini typeof positionO = " + typeO);
     // console.log("ini positionX = " + positionX + "| ini positionO = " + positionO);
+
+/* logic to know if positionO or positionX is number or undefined
+this need to be done if using slice function */
+
     if (typeX === "number" && typeO === "number") {
       if (positionO > positionX)
-      tempoArr = arr.slice(positionX, positionO)
+      tempoArr = arr.slice(positionX, positionO) // slice array of x with corresponding o
       else if (positionX > positionO) {
-        tempoArr = arr.slice(positionO, positionX)
+        tempoArr = arr.slice(positionO, positionX) // slice array of o with corresponding x
       }
     }
+
     // console.log("ini tempoArr = " + tempoArr);
-    tempoDistance = tempoArr.length;
+    tempoDistance = tempoArr.length; // function to obtain distance
     // console.log("ini tempoDistance = " + tempoDistance);
+
+    // logic to assign first distance between x and o or reverse
     if (shortestDistance === 0) {
       shortestDistance = tempoDistance;
       // console.log("ini shortestDistance pertama = " + shortestDistance);
-    } else if (tempoDistance < shortestDistance) {
+    }
+    // logic to compare tempoDistance and shortestDistance
+    else if (tempoDistance < shortestDistance) {
       // console.log("ini shortestDistance sebelum perbandingan = " + shortestDistance);
       shortestDistance = tempoDistance;
       // console.log("ini shortestDistance setelah perbandingan = " + shortestDistance);
     }
+
     // console.log("ini shortestDistance = " + shortestDistance);
     // console.log("--<>--");
+    
   }
   return shortestDistance;
 }
