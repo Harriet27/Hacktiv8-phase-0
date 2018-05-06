35f613923de570a24e71503e9a1861d96c6e34d4
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-05-06 20:08:43 +0700

    add pengelompokkan-angka.js file

---
+++ h8-p0-w3/pengelompokkan-angka.js
@@ -0,0 +1,39 @@
+function mengelompokkanAngka(arr) {
+
+// init variables
+  var arrEven = [];
+  var arrOdd = [];
+  var arrThree = [];
+  var arrResults =[arrEven, arrOdd, arrThree];
+
+  // console.log(arr);
+
+// loop to clasify even, odd, and multiplier of 3
+  for (var i = 0; i <= arr.length - 1; i++) {
+    var inspector = arr[i];
+
+    // console.log(inspector);
+
+    // classifier logic
+    if (inspector % 3 === 0) {
+      arrThree.push(inspector);
+      // console.log(arrThree);
+    } else if (inspector % 2 === 0) {
+      arrEven.push(inspector);
+      // console.log(arrEven);
+    } else if (inspector !== 0) {
+      arrOdd.push(inspector);
+      // console.log(arrOdd);
+    }
+  }
+
+  // console.log(arrResults);
+  return arrResultsn;
+
+}
+
+// TEST CASES
+console.log(mengelompokkanAngka([2, 4, 6])); // [ [2, 4], [], [6] ]
+console.log(mengelompokkanAngka([1, 2, 3, 4, 5, 6, 7, 8, 9])); // [ [ 2, 4, 8 ], [ 1, 5, 7 ], [ 3, 6, 9 ] ]
+console.log(mengelompokkanAngka([100, 151, 122, 99, 111])); // [ [ 100, 122 ], [ 151 ], [ 99, 111 ] ]
+console.log(mengelompokkanAngka([])); // [ [], [], [] ]
