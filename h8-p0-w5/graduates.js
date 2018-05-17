2d0116f157a46f3f69acc02e09a2a7b79f7d8cb6
Yusuf Salman Noerinaldy Siregar <yusufsiregar5@gmail.com>
2018-05-17 14:31:46 +0700

    add comments

--- h8-p0-w5/graduates.js
+++ h8-p0-w5/graduates.js
@@ -1,36 +1,47 @@
 function graduates(students) {
-  var objPassingStudent = {};
 
-  var scannedClassName = "";
+  // init var
+  var objPassingStudent = {}; // obj to contain students with passing grades
+  var scannedClassName = ""; // var to contain first letter of class name
 
+  // loop to access class names of students
   for (var i = 0; i < students.length; i++) {
-    var classNameInspector = students[i].class[0];
-    var tempoClassName = students[i].class;
+
+    // init var
+    var classNameInspector = students[i].class[0]; // var to contain first letter of student's class name
+    var tempoClassName = students[i].class;     // var to contain class name of students
     // console.log(classNameInspector);
+
+    /* logic to detect if class name is already included in objHighScoringStudent,
+    comparison using classNameInspector compared with scannedClassName
+    */
     if (scannedClassName.indexOf(classNameInspector) === -1) {
-      scannedClassName += classNameInspector[0];
-      objPassingStudent[tempoClassName] = [];
+      scannedClassName += classNameInspector[0]; // if new class name is detected, first letter is added to scannedClassName
+      objPassingStudent[tempoClassName] = []; // create new property at objPassingStudent named after class name
     }
   }
 
   // console.log(scannedClassName);
   // console.log(objPassingStudent);
 
+  // loop to access student information from students array
   for (var j = 0; j < students.length; j++) {
-    var studentInfoInspector = students[j];
-    var tempoClass = studentInfoInspector.class;
+
+    var studentInfoInspector = students[j]; // var to contain student info base from index of students array
+    var tempoClass = studentInfoInspector.class; // var to contain name of studennts class
     var objTempoStudentInfo = {
       name: '',
       score: 0
-    }
+    } // obj to contain name and score property of students
 
     // console.log(studentInfoInspector.class);
     // console.log(studentInfoInspector.score);
 
+    // logic to detect if students score is above 75 or not
     if (studentInfoInspector.score > 75) {
-      objTempoStudentInfo.name += studentInfoInspector.name;
-      objTempoStudentInfo.score += studentInfoInspector.score;
-      objPassingStudent[tempoClass].push(objTempoStudentInfo);
+      objTempoStudentInfo.name += studentInfoInspector.name; // assigning objTempoStudentInfo name property
+      objTempoStudentInfo.score += studentInfoInspector.score; // assigning objTempoStudentInfo score property
+      objPassingStudent[tempoClass].push(objTempoStudentInfo); // push objTempoStudentInfo to objPassingStudent property named after studnet's class
       // console.log(objTempoStudentInfo);
       // console.log(tempoClass);
     }
--- h8-p0-w5/highest-score.js
+++ h8-p0-w5/highest-score.js
@@ -1,34 +1,50 @@
 function highestScore(students) {
+
+  // init variables
   var objHighScoringStudent = {};
 
+  // logic to detect if arr is empty or not
   if (students.length === 0) {
     return objHighScoringStudent;
   } else {
 
+
+    // init var to contain first letter of class name
     var scannedClassName = "";
 
+    // loop to access class names of students
     for (var i = 0; i < students.length; i++) {
-      var classNameInspector = students[i].class[0];
-      var tempoClassName = students[i].class;
+
+      // init variables
+      var classNameInspector = students[i].class[0]; // var to contain first letter of student's class name
+      var tempoClassName = students[i].class; // var to contain class name of students
       // console.log(classNameInspector);
+
+      /* logic to detect if class name is already included in objHighScoringStudent,
+      comparison using classNameInspector compared with scannedClassName
+      */
       if (scannedClassName.indexOf(classNameInspector) === -1) {
-        scannedClassName += classNameInspector[0];
+        scannedClassName += classNameInspector[0]; // if new class name is detected, first letter is added to scannedClassName
         objHighScoringStudent[tempoClassName] = {
           name: "",
           score: 0
-        };
+        }; // creation of new property named after class name in objHighScoringStudent
         // console.log(className);
       }
 
     }
 
+    // loop to access student info
     for (var j = 0; j < students.length; j++) {
-      var studentInfoInspector = students[j];
-      var tempoClass = objHighScoringStudent[studentInfoInspector.class]
+
+      // init var
+      var studentInfoInspector = students[j]; // var to contain student info from students array
+      var tempoClass = objHighScoringStudent[studentInfoInspector.class] // var to acesss property named after student's class
 
       // console.log(studentInfoInspector.score);
       // console.log(tempoClass.score);
 
+      // logic to find highestScore
       if (studentInfoInspector.score > tempoClass.score) {
         tempoClass.name += studentInfoInspector.name;
         tempoClass.score += studentInfoInspector.score
